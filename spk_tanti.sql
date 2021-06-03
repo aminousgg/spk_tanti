99ea2aec0e6d20571d50205793ac9bdbcb94674c
aminousgg <nur.aminnudin@gmail.com>
2021-06-04 03:51:05 +0700

    sql

--- spk_tanti.sql
+++ spk_tanti.sql
@@ -27,7 +27,7 @@ CREATE TABLE `auth` (
   `status` tinyint(1) DEFAULT NULL,
   `level` enum('Admin','User') DEFAULT 'User',
   PRIMARY KEY (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `auth` */
 
@@ -37,7 +37,8 @@ insert  into `auth`(`id_user`,`username`,`password`,`token`,`status`,`level`) va
 (13,'aminousgg@gmail.com','aabff2636e4b909ef56bcbbd7c62064a',NULL,NULL,'User'),
 (14,'michelle@gmail.com','aabff2636e4b909ef56bcbbd7c62064a',NULL,NULL,'User'),
 (15,'admin@amin.com','aabff2636e4b909ef56bcbbd7c62064a',NULL,NULL,'Admin'),
-(16,'syahreza@gmail.com','ee25136ee204aa44362b5444bf6556f8',NULL,NULL,'User');
+(16,'syahreza@gmail.com','ee25136ee204aa44362b5444bf6556f8',NULL,NULL,'User'),
+(17,NULL,'d41d8cd98f00b204e9800998ecf8427e',NULL,NULL,'User');
 
 /*Table structure for table `pelamar` */
 
@@ -61,15 +62,16 @@ CREATE TABLE `pelamar` (
   `nilai` float(10,2) DEFAULT NULL,
   `range_nilai` int(2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `pelamar` */
 
 insert  into `pelamar`(`id`,`id_user`,`nama`,`nik`,`tgl_lahir`,`tmp_lahir`,`no_hp`,`alamat`,`gender`,`foto`,`img_ijasah`,`img_ktp`,`img_cv`,`img_sertif`,`nilai`,`range_nilai`) values 
-(1,10,'Nur Amin Nudin','3328132209980002','1998-09-22','Tegal','089667203086','Jl. Karya Bakti RT. 01/02 Kec. Dukuhturi','Laki-laki','10_me_persegi.jpg','10_Ijazah.jpg','10_Scan_KTP.pdf','10_mycv.pdf',NULL,3.40,4),
+(1,10,'Nur Amin Nudin','3328132209980002','1998-09-22','Tegal','089667203086','Jl. Karya Bakti RT. 01/02 Kec. Dukuhturi','Laki-laki','10_me_persegi.jpg','10_Ijazah.jpg','10_Scan_KTP.pdf','10_mycv.pdf',NULL,3.50,4),
 (2,12,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'default_profile.svg',NULL,NULL,NULL,NULL,NULL,NULL),
 (3,14,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'default_profile.svg',NULL,NULL,NULL,NULL,NULL,NULL),
-(6,16,'Syahreza Panca','8174724','2001-02-15','Semarang','0899872181','Jl. Sawung 3','Laki-laki','16_michellearabelle_profilepicture.jpg',NULL,NULL,NULL,NULL,NULL,NULL);
+(6,16,'Syahreza Panca','8174724','2001-02-15','Semarang','0899872181','Jl. Sawung 3','Laki-laki','16_michellearabelle_profilepicture.jpg',NULL,NULL,NULL,NULL,2.80,4),
+(7,17,NULL,NULL,NULL,NULL,NULL,NULL,NULL,'default_profile.svg',NULL,NULL,NULL,NULL,NULL,NULL);
 
 /*Table structure for table `pendidikan` */
 
@@ -84,7 +86,7 @@ CREATE TABLE `pendidikan` (
   `jenis_pend` enum('Formal','Non') DEFAULT NULL,
   `bidang` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `pendidikan` */
 
@@ -96,7 +98,8 @@ insert  into `pendidikan`(`id`,`id_user`,`tingkat`,`nama`,`tahun`,`jenis_pend`,`
 (8,10,'SMP','MTs. Assalafiyah Kota Tegal',2013,'Formal','-'),
 (9,16,'SD','SD 2 Banyumanik',2010,'Formal','-'),
 (10,16,'SMP','SMP 12 Smarang',2013,'Formal','-'),
-(11,16,'SMA','SMA 3 Semarang',2016,'Formal','-');
+(11,16,'SMA','SMA 3 Semarang',2016,'Formal','-'),
+(12,17,NULL,NULL,NULL,NULL,NULL);
 
 /*Table structure for table `riwayat_kerja` */
 
@@ -111,7 +114,7 @@ CREATE TABLE `riwayat_kerja` (
   `tahun_selesai` year(4) DEFAULT NULL,
   `gaji` double(12,2) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
 
 /*Data for the table `riwayat_kerja` */
 
@@ -119,7 +122,9 @@ insert  into `riwayat_kerja`(`id`,`id_user`,`nama_perusahaan`,`jabatan`,`tahun_m
 (1,10,'PT Rajawali Nusindo','Web Developer Divisi IT',2020,2021,4100000.00),
 (2,12,NULL,NULL,NULL,NULL,NULL),
 (3,14,NULL,NULL,NULL,NULL,NULL),
-(5,16,NULL,NULL,NULL,NULL,NULL);
+(5,16,'PT Rajawali Nusindo','Web Developer Divisi IT',2020,2021,909009099.00),
+(6,17,NULL,NULL,NULL,NULL,NULL),
+(8,10,'PT Rajawali asds','Web Developer Divisi IT',2021,2022,2342542.00);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
